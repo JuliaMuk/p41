ff82c2621a7bf43489b6fcf2621a1fd1e2c6f9d8
lector <lector@248-TEACHER>
2024-10-16 15:14:21 +0500

    install breeze

--- resources/views/student/edit.blade.php
+++ resources/views/student/edit.blade.php
@@ -6,19 +6,19 @@
   @method('put')
     <div class="grid gap-6 mb-6 md:grid-cols-2">
       <div>
-        <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-        <input name="fname"  type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{$student->fname}}" required />
+        <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">First name</label>
+        <input name="fname"  type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{$student->fname}}" required />
       </div>
       <div>
-        <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
-        <input name="lname" type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{$student->lname}}" required />
+        <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900">Last name</label>
+        <input name="lname" type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{$student->lname}}" required />
       </div>
       <div>
-        <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Возраст</label>
-        <input name="age" type="number" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="{{$student->age}}" required />
+        <label for="company" class="block mb-2 text-sm font-medium text-gray-900">Возраст</label>
+        <input name="age" type="number" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{$student->age}}" required />
       </div>
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Обновить</button>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Обновить</button>
   </form>
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\StudentController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,13 +22,21 @@ Route::get('/', function () {
 
 Route::get('/first', [MainController::class, 'show'])->name('first');
 
-Route::get('/students', [StudentController::class, 'index'])->name('student.index');
-
-Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('student.destroy');
 
 
-Route::post('/students',[StudentController::class,'store'])->name('students.store');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/students', [StudentController::class, 'index'])->name('student.index');
+    Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('student.destroy');
+    Route::post('/students',[StudentController::class,'store'])->name('students.store');
+    Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
+    Route::put('/students/{student}',[StudentController::class,'update'])->name('students.update');
+});
 
-Route::put('/students/{student}',[StudentController::class,'update'])->name('students.update');
+require __DIR__.'/auth.php';
